726d4b5ed3e6bca51955caec37e6adf51f605c61
Salama AB <aksalj@aksalj.com>
2017-06-22 09:07:36 +0300

    Clean up; prep server and account service

--- test/grpc.js
+++ test/grpc.js
@@ -1,17 +1,41 @@
 'use strict';
 
-var should = require('should');
-var validate = require('validate.js');
-var fixtures = require('./fixtures.local');
+const should = require('should');
+const validate = require('validate.js');
+const fixtures = require('./fixtures.local');
+const ATServer = require('../lib/server');
 
 describe('gRPC', function () {
     this.timeout(5000);
+    let server = null;
 
-    it('starts server', function (done) {
+    before(() => {
+        server = new ATServer(fixtures.TEST_ACCOUNT);
+        server.start();
+    });
 
-        var ATServer = require('../lib/server');
-        var server = new ATServer(fixtures.TEST_ACCOUNT);
+    it('generates, authenticates and revokes tokens', function (done) {
+        const token = server.generateToken();
+        token.should.be.a.string();
+        const valid = server.authenticate(token);
+        valid.should.be.exactly(true);
+        server.revoke(token);
+        const invalid = server.authenticate(token);
+        invalid.should.be.exactly(false);
+    });
 
+    it('has an instance of the lib', function (done) {
+        var p = server.getATInstance().fetchAccount();
+        validate.isPromise(p).should.be.exactly(true);
+
+        p.then(function (resp) {
+            resp.should.have.property('UserData');
+            done();
+
+        }).catch(function (error) {
+            console.log(error);
+            done();
+        });
     });   
 });
 
